734d9fef2fc99d1a0564214c4f50daeb7daa6a00
georgy <shchedrin.georgy@gmail.com>
2020-10-08 17:23:02 +0300

    GULP PROJECT MAJOR REFACTORING

--- gulpfile.js
+++ gulpfile.js
@@ -1,143 +1,269 @@
-//Подключаем галп
-const gulp = require('gulp');
-//Объединение файлов
-const concat = require('gulp-concat');
-//Добавление префиксов
-const autoprefixer = require('gulp-autoprefixer');
-//Минификация стилей
-const cssnano = require('gulp-cssnano');
-//Перенос @media
-const mqpacker = require('css-mqpacker');
-//Оптимизация скриптов
-const uglify = require('gulp-uglify');
-//Удаление файлов
-const del = require('del');
-//Синхронизация с браузером
-const browserSync = require('browser-sync').create();
-//Для препроцессоров стилей
-const sourcemaps = require('gulp-sourcemaps');
-//Sass препроцессор
-const sass = require('gulp-sass');
-//Модуль для сжатия изображений
-const imagemin = require('gulp-imagemin');
-const mozjpeg = require('imagemin-mozjpeg');
-const pngquant = require('imagemin-pngquant');
-//Модуль для создания svg спрайта
-const svgstore = require('gulp-svgstore');
-const svgmin = require('gulp-svgmin');
-//Модуль переименовывания файлов
-const rename = require('gulp-rename');
-const postcss = require("gulp-postcss");
+"use strict";
 
-//Порядок подключения файлов со стилями
-const styleFiles = [
-   './source/scss/color.scss',
-   './source/scss/main.scss'
-]
-//Порядок подключения js файлов
-const scriptFiles = [
-   './source/js/lib.js',
-   './source/js/main.js'
-]
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+// var sourcemaps = require("gulp-sourcemaps");
 
-//Таск для обработки стилей
-gulp.task('styles', () => {
-   //Шаблон для поиска файлов CSS
-   //Всей файлы по шаблону './source/css/**/*.css'
-   return gulp.src('./source/scss/**/*.+(scss|sass)')
-      .pipe(sourcemaps.init())
-      //Указать stylus() , sass() или less()
-      .pipe(sass())
-      //Объединение файлов в один
-      //.pipe(concat('style.css'))
-      //Добавить префиксы
-      .pipe(autoprefixer({
-         overrideBrowserslist: ['last 2 versions'],
-         cascade: false
-      }))
-      //Сортировка @media
-     .pipe(postcss([
-         mqpacker ({
-             sort: true
-         })
-      ]))
-      //Минификация CSS
-      .pipe(cssnano({
-            discardComments: {removeAll: true}
-        }))
-      .pipe(rename({
-         suffix: '.min'
-      }))
-      //Выходная папка для стилей
-      .pipe(gulp.dest('./css'))
-      .pipe(browserSync.stream());
+var csscomb = require("gulp-csscomb");
+var csslint = require("gulp-csslint");
+
+var server = require("browser-sync").create();
+var rename = require("gulp-rename");
+
+var pump = require("pump");
+// var uglify = require("gulp-uglify");
+const terser = require('gulp-terser');
+
+var concat = require("gulp-concat");
+
+var htmlmin = require("gulp-htmlmin")
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+
+var minify = require("gulp-csso");
+var imagemin = require("gulp-imagemin");
+var imageminJpegRecompress = require("imagemin-jpeg-recompress");
+var pngquant = require("imagemin-pngquant");
+var cache = require("gulp-cache");
+var webp = require("gulp-webp");
+var svgstore = require("gulp-svgstore");
+var svgmin = require("gulp-svgmin");
+
+var changed = require("gulp-changed");
+var del = require("del");
+
+//var ghpages = require("gulp-gh-pages");
+var ghpages = require('gh-pages');
+
+
+gulp.task("compress", function (cb) {
+  pump([
+    gulp.src("source/js/**/*.js"),
+    // sourcemaps.init(),
+    terser(),
+    rename({suffix: ".min"}),
+    // sourcemaps.write(),
+    gulp.dest("build/js")
+    ], cb);
 });
 
-//Таск для обработки скриптов
-gulp.task('scripts', () => {
-   //Шаблон для поиска файлов JS
-   //Всей файлы по шаблону './source/js/**/*.js'
-   return gulp.src(scriptFiles)
-      //Объединение файлов в один
-      .pipe(concat('main.js'))
-      //Минификация JS
-      .pipe(uglify({
-         toplevel: true
-      }))
-      .pipe(rename({
-         suffix: '.min'
-      }))
-      //Выходная папка для скриптов
-      .pipe(gulp.dest('./js'))
-      .pipe(browserSync.stream());
+gulp.task("concat", gulp.series("compress", function (cb) {
+  pump([
+    gulp.src(["source/js/*.js"]),
+    // sourcemaps.init(),
+    terser(),
+    concat("main.min.js"),
+    // sourcemaps.write(),
+    gulp.dest("build/js")
+  ], cb);
+}));
+
+gulp.task("style", function() {
+  return gulp.src("source/sass/style.scss")
+    .pipe(plumber())
+    // .pipe(sourcemaps.init())
+    .pipe(sass())
+    // .pipe(sourcemaps.write({includeContent: false}))
+    // .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    // .pipe(sourcemaps.write())
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
 });
 
-//Таск для очистки папки build
-gulp.task('del', () => {
-   return del(['css/*', 'img/*', 'js/*'])
+gulp.task("csscomb", function () {
+  return gulp.src("build/css/**/*.css")
+    .pipe(csscomb("build/csscomb.json"))
+    .pipe(gulp.dest("build/css"));
 });
 
-//Таск для сжатия изображений
-gulp.task('img-compress', ()=> {
-   return gulp.src('./source/img/**')
-   .pipe(imagemin([
-      pngquant(),
-      mozjpeg({
-         progressive: true
+gulp.task("watch", function () {
+  server.init({
+    server: "build/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("style"));
+  gulp.watch("source/*.html", gulp.series("html")).on("change", server.reload);
+  gulp.watch("source/js/*.js", gulp.series("concat")).on("change", server.reload);
+});
+
+// gulp.task("watch", function() {
+//   gulp.watch("source/sass/**/*.{scss,sass}", ["style"]).on("change", server.reload);
+//   gulp.watch("source/js/**/*.js").on("change", server.reload);
+//   gulp.watch("source/*.html", ["html"]).on("change", server.reload);
+// });
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(plumber())
+    // .pipe(sourcemaps.init())
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(htmlmin({
+      minifyJS: true,
+      minifyURLs: true,
+      collapseWhitespace: false,
+      removeComments: true,
+      sortAttributes: true,
+      sortClassName: true
+    }))
+    // .pipe(sourcemaps.write())
+    .pipe(gulp.dest("build"));
+});
+
+// Images optimization and copy in /build
+gulp.task("images", function() {
+  return gulp.src("source/img/**/*.*", "!source/img/raster/dont_optimize/*.*")
+    .pipe(imagemin([
+//      imagemin.gifsicle({interlaced: true}),
+      imagemin.mozjpeg({progressive: true}),
+      imageminJpegRecompress({
+        loops: 5,
+        min: 65,
+        max: 70,
+        quality:"medium"
+      }),
+      imagemin.svgo(),
+      imagemin.optipng({optimizationLevel: 3}),
+      pngquant({quality: [0.65, 0.7], speed: 5})
+    ],{
+      verbose: true
+    }))
+    .pipe(gulp.dest("source/img/min"));
+});
+
+gulp.task("del-images", function () {
+  return del("source/img/min");
+});
+
+gulp.task("minimg", gulp.series("del-images", "images", function (done) {
+	done();
+}));
+
+
+// Clearing the cache
+gulp.task("clearcache", function (done) {
+  return cache.clearAll(done);
+});
+
+gulp.task("oldimages", function () {
+  return gulp.src("source/img/raster/*.{jpg,png}")
+    .pipe(changed("source/img/min/raster"))
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img/raster"));
+});
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/raster/*.{jpg,png}")
+    .pipe(webp({quality: 85}))
+    .pipe(gulp.dest("build/img/webp"));
+});
+
+gulp.task("copy-images", function() {
+  return gulp.src([
+    "source/img/min/**/*.*"
+    ], {
+      base: "source/img/min"
+    })
+      .pipe(gulp.dest("build/img"))
+    //return del("build/img/min/");
+});
+
+gulp.task("copy-not-opitmized-images", function() {
+  return gulp.src([
+    "source/img/raster/dont_optimize/*.*"
+    ], {
+      base: "source/img/raster/dont_optimize"
+    })
+      .pipe(gulp.dest("build/img/raster/"))
+    //return del("build/img/min/");
+});
+
+
+gulp.task("svg", function () {
+  return gulp.src("source/img/vector/**/*.svg")
+    .pipe(svgmin())
+    .pipe(gulp.dest("build/img/vector"));
+});
+
+gulp.task("sprite", gulp.series("svg", function () {
+  return gulp.src("build/img/vector/sprite/*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+      }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img/vector"));
+}));
+
+gulp.task("del-sprite", function () {
+  return del("build/img/vector/sprite{.svg,/}");
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+      "source/fonts/**/*.{woff,woff2}"
+      //"source/img/**",
+      //"source/js/**/*.js"
+    ], {
+      base: "source"
+    })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("cpcomb", function () {
+  return gulp.src([
+      "./csscomb.json"], {
+        base: "./"
       })
-   ],{
-         verbose: true
-   }))
-   .pipe(gulp.dest('./img/'))
+  .pipe(gulp.dest("build"));
 });
 
-//Таск для создания svg спрайта
-gulp.task('svgsprite', ()=> {
-   return gulp.src('./source/svgsprite/*.svg')
-   .pipe(svgmin())
-   .pipe(svgstore())
-   .pipe(rename({basename: 'sprite'}))
-   .pipe(gulp.dest('./img/'))
+gulp.task("clean", function () {
+  return del("build");
 });
 
-//Таск для отслеживания изменений в файлах
-gulp.task('watch', () => {
-   browserSync.init({
-      server: {
-         baseDir: "./"
-      }
-   });
-   //Следить за добавлением новых изображений
-   gulp.watch('./source/img/**', gulp.series('img-compress'))
-   //Следить за добавлением новых svg
-   gulp.watch('./source/svg/**', gulp.series('svgsprite'))
-   //Следить за файлами со стилями с нужным расширением
-   gulp.watch('./source/scss/**/*.scss', gulp.series('styles'))
-   //Следить за JS файлами
-   gulp.watch('./source/js/**/*.js', gulp.series('scripts'))
-   //При изменении HTML запустить синхронизацию
-   gulp.watch("./*.html").on('change', browserSync.reload);
-});
+gulp.task("build", gulp.series(
+	//"del-images" - удаляем исходную папку со сжатыми img - идет отдельно, первым (альтернатива - minimg)
+  //"images" - заново сжимаем все img - идет отдельно, вторым (альтернатива - minimg)
+  "minimg",
+	"clean",
+	"clearcache",
+	"copy",
+	//"cpcomb",
+  "copy-images",
+  "copy-not-opitmized-images",
+	"webp",
+	"sprite",
+	"style",
+	//"csscomb",// - запускается отдельно, при желании
+	"html",
+	"concat",
+	function (done) {
+		done();
+}));
 
-//Таск по умолчанию, Запускает del, styles, scripts, img-compress, svgsprite и watch
-gulp.task('default', gulp.series('del', gulp.parallel('styles', 'scripts', 'img-compress', 'svgsprite'), 'watch'));
+//gulp.task('deploy', function() {
+//  return gulp.src('./build/**/*')
+//    .pipe(ghpages());
+//});
+
+gulp.task('deploy', function() {
+	return ghpages.publish('build', function(err) {});
+});
